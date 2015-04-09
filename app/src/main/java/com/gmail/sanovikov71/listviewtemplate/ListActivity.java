ef352f710c8758b32380955c2c987a9a4e1586aa
unknown <senovik@n102533.merann.ru>
2015-04-09 19:21:21 +0300

    initial android studio commit

---
+++ app/src/main/java/com/gmail/sanovikov71/listviewtemplate/ListActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.sanovikov71.listviewtemplate;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+}
